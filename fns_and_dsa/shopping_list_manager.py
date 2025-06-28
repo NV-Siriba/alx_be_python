75b2fa4b9e5328508bb9ae0a54fb099579f87dca
NV-Siriba <161623658+NV-Siriba@users.noreply.github.com>
2025-06-29 00:52:49 +0300

    Add files via upload

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,35 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            # Prompt for and add an item
+            item_add=input('Enter an item to add to your list:')
+            shopping_list.append(item_add)
+
+        
+        elif choice == '2':
+            # Prompt for and remove an item
+            item_remove=input('Enter an item to remove from your list:')
+            shopping_list.remove(item_remove)
+        elif choice == '3':
+            # Display the shopping list
+            for display in shopping_list:
+                print(display)
+            
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
