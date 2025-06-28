631f2c0350054a915c9318a732a0fa41cd31881f
NV-Siriba <161623658+NV-Siriba@users.noreply.github.com>
2025-06-28 21:52:23 +0300

    Add files via upload

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,16 @@
+def perform_operation(num1:float,num2:float,operation:str):
+   
+    if operation=='add':
+        return(num1+num2)
+    elif operation=='subtract':
+        return(num1-num2)
+    elif operation=='divide':
+        if num2==0:
+            raise ValueError('cannot divide by zero')
+        return(num1/num2)
+    elif operation=='multiply':
+        return(num1*num2)
+    else:
+         raise ValueError("Unsupported operation. Use add,subtract,divide or multiply")
+print(perform_operation(5,0,'divide'))
+
