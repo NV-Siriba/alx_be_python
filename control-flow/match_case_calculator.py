94ceb07b7e42920ebc2b66171920d1b124bdfe34
NV-Siriba <161623658+NV-Siriba@users.noreply.github.com>
2025-07-01 21:27:16 +0300

    Update match_case_calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,17 +1,18 @@
-num1=int(input("enter your first number:"))
-num2=int(input("enter your second number:"))
-oparation=input("Choose the operation (+, -, *, /):")
-match oparation:
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number:"))
+operation = input("Choose the operation (+, -, *, /):")
+
+match operation:
     case "+":
-        print(num1+num2)
+        print(num1 + num2)
     case "-":
-        print(num1-num2)
+        print(num1 - num2)
     case "*":
-        print(num1*num2)
+        print(num1 * num2)
     case "/":
         try:
-            print(num1/num2)
+            print(num1 / num2)
         except ZeroDivisionError:
             print("Error: Cannot divide by zero.")
-    case "_":
-        print("oparation not available")
+    case _:
+        print("operation not available")
