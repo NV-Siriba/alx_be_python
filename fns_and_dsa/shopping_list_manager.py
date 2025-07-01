2951615869c86fb365f55c9b6b5e1b84db9a6f46
NV-Siriba <161623658+NV-Siriba@users.noreply.github.com>
2025-07-01 22:11:05 +0300

    Update shopping_list_manager.py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -12,7 +12,7 @@ def main():
 
         if choice == '1':
             # Prompt for and add an item
-            item_add=input('Enter an item to add to your list:')
+            item_add=input('Enter the item to add:')
             shopping_list.append(item_add)
 
         
