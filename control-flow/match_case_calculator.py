17ddd8558a276868d31c75773da23784cb475c51
NV-Siriba <161623658+NV-Siriba@users.noreply.github.com>
2025-07-01 21:31:49 +0300

    Update match_case_calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -4,14 +4,14 @@ operation = input("Choose the operation (+, -, *, /):")
 
 match operation:
     case "+":
-        print(num1 + num2)
+        print(f'The result is{num1 + num2}')
     case "-":
-        print(num1 - num2)
+        print(f'The result is{num1 - num2}')
     case "*":
-        print(num1 * num2)
+        print(f'The result is{num1 * num2}')
     case "/":
         try:
-            print(num1 / num2)
+            print(f'The result is{num1 / num2}')
         except ZeroDivisionError:
             print("Error: Cannot divide by zero.")
     case _:
