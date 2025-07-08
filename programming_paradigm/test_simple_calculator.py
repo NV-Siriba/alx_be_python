5cec26c3f6c52c808b670fe47285fc5e876a629f
NV-Siriba <161623658+NV-Siriba@users.noreply.github.com>
2025-07-08 11:10:47 +0300

    Update test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -11,11 +11,11 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(3,1),2)
         self.assertEqual(self.calc.subtract(-1,-1),0)
         self.assertEqual(self.calc.subtract(3,-5),8)
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(3,2),6)
         self.assertEqual(self.calc.multiply(-1,-1),1)
         self.assertEqual(self.calc.multiply(3,-5),-15)
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(2,1),2)
         self.assertEqual(self.calc.divide(-1,-1),1)
         self.assertEqual(self.calc.divide(-2,2),-1)
